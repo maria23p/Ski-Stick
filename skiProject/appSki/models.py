3e33e4499534a24e5f12a050022faece3763bec5
MonicaSangrador <113512150+MonicaSangrador@users.noreply.github.com>
2023-10-31 13:39:17 +0100

    clases

--- skiProject/appSki/models.py
+++ skiProject/appSki/models.py
@@ -3,12 +3,16 @@ from django.db import models
 class Localidad(models.Model):
     # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
     nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
 
 class Pista(models.Model):
     nombre = models.CharField(max_length=50)
     color = models.CharField(max_length=50)
     longitud = models.FloatField
     estado = models.CharField(max_length = 50)
+    def __str__(self):
+        return self.nombre
 
 class Servicio(models.Model):
     nombre = models.CharField(max_length=50)
@@ -16,6 +20,8 @@ class Servicio(models.Model):
     horario = models.TimeField
     capacidad = models.IntegerField
     reserva = models.BooleanField
+    def __str__(self):
+        return self.nombre
 
 class Estacion(models.Model):
     # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
@@ -28,3 +34,5 @@ class Estacion(models.Model):
     telefono = models.IntegerField()
     estado = models.CharField(max_length = 50)
     servicio = models.ManyToManyField(Servicio)
+    def __str__(self):
+        return self.nombre
