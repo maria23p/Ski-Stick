436f90ab3b5c74f6e19223d65fa3bf6dff561245
MonicaSangrador <113512150+MonicaSangrador@users.noreply.github.com>
2023-10-31 12:47:47 +0100

    urls

--- skiProject/appSki/urls.py
+++ skiProject/appSki/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
- path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('appSki/', include('appSki.urls')),
+    path('admin/', admin.site.urls),
 ]
