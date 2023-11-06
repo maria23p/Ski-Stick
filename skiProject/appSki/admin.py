90cc716972fb03b80931e152d9900ac9ed70bbf0
maria23p <126978708+maria23p@users.noreply.github.com>
2023-11-06 13:06:15 +0100

    cambio models

--- skiProject/appSki/admin.py
+++ skiProject/appSki/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Localidad, Pista, Servicio, Estacion
 
 # Register your models here.
+
+admin.site.register(Localidad)
+admin.site.register(Pista)
+admin.site.register(Servicio)
+admin.site.register(Estacion)
+
--- skiProject/appSki/models.py
+++ skiProject/appSki/models.py
@@ -9,17 +9,16 @@ class Localidad(models.Model):
 class Pista(models.Model):
     nombre = models.CharField(max_length=50)
     color = models.CharField(max_length=50)
-    longitud = models.FloatField
     estado = models.CharField(max_length = 50)
     def __str__(self):
         return self.nombre
 
 class Servicio(models.Model):
     nombre = models.CharField(max_length=50)
-    costo = models.FloatField
-    horario = models.TimeField
-    capacidad = models.IntegerField
-    reserva = models.BooleanField
+    costo = models.FloatField()
+    horario = models.TimeField()
+    capacidad = models.IntegerField()
+    reserva = models.BooleanField()
     def __str__(self):
         return self.nombre
 
@@ -27,10 +26,10 @@ class Estacion(models.Model):
     # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
     nombre = models.CharField(max_length=50)
     localidad = models.ForeignKey(Localidad, on_delete=models.CASCADE)
-    superficie = models.FloatField
+    superficie = models.FloatField()
     pista = models.ManyToManyField(Pista)
-    precio_dia = models.FloatField
-    horario = models.TimeField
+    precio_dia = models.FloatField()
+    horario = models.TimeField()
     telefono = models.IntegerField()
     estado = models.CharField(max_length = 50)
     servicio = models.ManyToManyField(Servicio)
