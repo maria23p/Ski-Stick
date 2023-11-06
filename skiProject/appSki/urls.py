733f2ff30a72ec1b551747cf1f68c4e10080ffa8
MonicaSangrador <113512150+MonicaSangrador@users.noreply.github.com>
2023-11-06 17:16:45 +0100

    vistas

--- skiProject/appSki/urls.py
+++ skiProject/appSki/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('', views.index, name='index'),
+ path('', views.index_localidades, name='index'),
+ path('localidades/<int:localidad_id>/', views.show_localidad, name='detail'),
+ path('localidades/<int:localidad_id>/estaciones', views.index_estaciones, name='estaciones'),
 ]
--- skiProject/appSki/views.py
+++ skiProject/appSki/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, get_list_or_404
+from .models import Localidad, Estacion
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Listado de estaciones de Esquí:")
+    return HttpResponse("Listado de Localidades:")
+
+
+#devuelve el listado de estaciones de esqui
+def index_localidades(request):
+	localidades = get_list_or_404(Localidad.objects.order_by('nombre'))
+	output = ', '.join([l.nombre for l in localidades])
+	return HttpResponse(output)
+
+#devuelve los datos de una localidad
+def show_localidad(request, localidad_id):
+	localidad = get_object_or_404(Localidad, pk=localidad_id)
+	output = f'Detalles de la localidad: {localidad.id}, {localidad.nombre}'
+	return HttpResponse(output)
+
+#devuelve las estaciones de cada localidad
+def index_estaciones(request, localidad_id):
+	localidad = get_object_or_404(Localidad, pk=localidad_id)
+	output = ', '.join([e.nombre for e in localidad.estacion_set.all()])
+	return HttpResponse(output)
+
+#devuelve los datos de una estacion
+def show_estacion(request, estacion_id):
+	estacion = get_object_or_404(Estacion, pk=estacion_id)
+	output = f'Detalles de la estacion: {estacion.id}, {estacion.nombre}, {estacion.superficie}, {estacion.precio_dia}, {estacion.horario}, {estacion.telefono}, {estacion.estado}. Pistas: {[p.nombre for p in estacion.pistas.all()]}. Servicios: {[s.nombre for s in estacion.servicios.all()]}'
+	return HttpResponse(output)
+
+
