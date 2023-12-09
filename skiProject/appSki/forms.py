838ec8c37f9b00d52758c969b163fd6b42b95dc7
maria23p <126978708+maria23p@users.noreply.github.com>
2023-12-09 19:05:21 +0100

    validacion correccion

--- skiProject/appSki/forms.py
+++ skiProject/appSki/forms.py
@@ -12,17 +12,17 @@ class vFormulario(forms.ModelForm):
     def clean_nombre(self):
         nombre = self.cleaned_data['nombre']
         if len(nombre) <= 2 or len(nombre) > 30:
-            raise forms.ValidationError("El nombre debe tener entre 3 y 30 caracteres.")
+            raise forms.ValidationError("El nombre debe tener entre 2 y 30 caracteres.")
         return nombre
     
     def clean_apellidos(self):
         apellidos = self.cleaned_data['apellidos']
         if len(apellidos) <= 2 or len(apellidos) > 30:
-            raise forms.ValidationError("Los apellidos deben tener entre 3 y 30 caracteres.")
+            raise forms.ValidationError("Los apellidos deben tener entre 2 y 30 caracteres.")
         return apellidos
     
     def clean_edad(self):
         edad = self.cleaned_data['edad']
-        if edad <= 0 or edad > 100:
-            raise forms.ValidationError("La edad debe estar entre 1 y 100 años.")
+        if edad <= 15 or edad > 90:
+            raise forms.ValidationError("La edad debe estar entre 15 y 90 años.")
         return edad
